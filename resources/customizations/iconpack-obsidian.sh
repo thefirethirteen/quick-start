d094bde628f0c1c5425b3d055f73cde0949106c0
thefirethirteen <78559591+thefirethirteen@users.noreply.github.com>
2021-02-04 23:31:25 +0200

    general fixes

--- resources/customizations/iconpack-obsidian.sh
+++ resources/customizations/iconpack-obsidian.sh
@@ -1,19 +1,15 @@
 # iconpack-obsidian.sh
-# version 7
-
-cd iconpack-obsidian
+# version 8
 
 echo -e "\e[1;45mAdding iconpack-obsidian! \e[0m"
 
-cd temp
-
 git clone "https://github.com/madmaxms/iconpack-obsidian"
+
 cd iconpack-obsidian
 sudo mv Obsidian* /usr/share/icons/
 cd ..
-rm -rf iconpack-obsidian
 
-cd ..
+rm -rf iconpack-obsidian
 
 echo -e "\e[1;32miconpack-obsidian has been installed to /usr/share/icons \e[0m"
 echo -e "\e[1;32miconpack-obsidian has been added! \e[0m"
