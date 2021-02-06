9d4a518a8ec97a5689111dc8afd190d9eba04963
thefirethirteen <78559591+thefirethirteen@users.noreply.github.com>
2021-02-07 00:14:41 +0200

    bump version to 1.1.2

--- resources/runtimes/java.sh
+++ resources/runtimes/java.sh
@@ -1,8 +1,8 @@
 # java.sh
-# version 1.1.1
+# version 1.1.2
 
 echo -e "\e[1;45mInstalling the latest OpenJDK! \e[0m"
 
-sudo apt-get -qqy install default-jdk
+sudo apt-get -qq -y install default-jdk
 
 echo -e "\e[1;32mOpenJDK has been installed! \e[0m"
