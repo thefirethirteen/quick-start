e981c4355ff2e1fe60607eb1860334ad65df9b17
thefirethirteen <78559591+thefirethirteen@users.noreply.github.com>
2021-02-07 00:01:24 +0200

    bump version to 1.1.0
    split 2 lines into 4 lines
    removed the echo on line 7, to make the script quiet
    added the "-qq" tag to apt-get (was missing)

--- resources/apps/streaming/spotify.sh
+++ resources/apps/streaming/spotify.sh
@@ -1,12 +1,13 @@
 # spotify.sh
-# version 1.0.1
+# version 1.1.0
 
 echo -e "\e[1;45mInstalling Spotify! \e[0m"
 
-curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg
+sudo apt-key -qq add -
+sudo tee /etc/apt/sources.list.d/spotify.list
 
-sudo apt-get update
+sudo apt-get update -qq
 
 sudo apt-get install -qqy spotify-client
 
