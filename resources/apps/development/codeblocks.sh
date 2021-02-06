83041eb8e1f5ccd9654e3ff478e63a90375839fb
thefirethirteen <78559591+thefirethirteen@users.noreply.github.com>
2021-02-06 23:05:23 +0200

    bump version to 1.0.1
    made "apt-get install" quiet

--- resources/apps/development/codeblocks.sh
+++ resources/apps/development/codeblocks.sh
@@ -1,12 +1,12 @@
 # codeblocks.sh
-# version 1.0.0
+# version 1.0.1
 
 echo -e "\e[1;45mInstalling Code::Blocks! \e[0m"
 
 sudo add-apt-repository -y ppa:codeblocks-devs/release
 sudo apt-get update
 
-sudo apt-get install -y codeblocks codeblocks-contrib
+sudo apt-get install -qqy codeblocks codeblocks-contrib
 
 sudo add-apt-repository -ry ppa:codeblocks-devs/release
 
