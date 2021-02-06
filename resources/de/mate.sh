c6d06d81f1cb1f6dc1d0b902e1a0f5450ccd3381
thefirethirteen <78559591+thefirethirteen@users.noreply.github.com>
2021-02-06 23:21:49 +0200

    bump version to 1.0.1
    added -qq flag to "apt-get install" (was missing)

--- resources/de/mate.sh
+++ resources/de/mate.sh
@@ -1,8 +1,8 @@
 # mate.sh
-# version 1.0.0
+# version 1.0.1
 
 echo -e "\e[1;45mInstalling the Mate Desktop Environment \e[0m"
 
-sudo apt install -y mate-desktop-environment mate-desktop-environment-extras ubuntu-mate-themes
+sudo apt install -qqy mate-desktop-environment mate-desktop-environment-extras ubuntu-mate-themes
 
 echo -e "\e[1;32mThe Mate Desktop Environment has been installed! \e[0m"
