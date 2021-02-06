c710f6b84f06c442097a1c6927e55d9c9010e9a7
thefirethirteen <78559591+thefirethirteen@users.noreply.github.com>
2021-02-06 23:57:45 +0200

    bump version to 1.0.2
    added the "-qq" tag to add-apt-repository (was missing)
    added the "-y" tag to add-apt-repository (was missing)

--- resources/apps/development/codeblocks.sh
+++ resources/apps/development/codeblocks.sh
@@ -1,13 +1,13 @@
 # codeblocks.sh
-# version 1.0.1
+# version 1.0.2
 
 echo -e "\e[1;45mInstalling Code::Blocks! \e[0m"
 
-sudo add-apt-repository -y ppa:codeblocks-devs/release
+sudo add-apt-repository -qqy ppa:codeblocks-devs/release
 sudo apt-get update
 
 sudo apt-get install -qqy codeblocks codeblocks-contrib
 
-sudo add-apt-repository -ry ppa:codeblocks-devs/release
+sudo add-apt-repository -qqy -r ppa:codeblocks-devs/release
 
 echo -e "\e[1;32mCode::Blocks has been installed! \e[0m"
