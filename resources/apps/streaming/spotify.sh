a10adf2a3f3c6d314ace9b3fd70a3ef03ba383c0
thefirethirteen <78559591+thefirethirteen@users.noreply.github.com>
2021-02-06 23:10:28 +0200

    bump version to 1.0.1
    made "apt-get install" quiet
    added the "-y" flag to "apt-get install" (was missing)

--- resources/apps/streaming/spotify.sh
+++ resources/apps/streaming/spotify.sh
@@ -1,12 +1,13 @@
 # spotify.sh
-# version 1.0.0
+# version 1.0.1
 
 echo -e "\e[1;45mInstalling Spotify! \e[0m"
 
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+
 sudo apt-get update
 
-sudo apt-get install spotify-client
+sudo apt-get install -qqy spotify-client
 
 echo -e "\e[1;32mSpotify has been installed! \e[0m"
