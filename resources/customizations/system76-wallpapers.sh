8e649fdb2a1735f0c5fc68521b035c9ef9b667df
thefirethirteen <78559591+thefirethirteen@users.noreply.github.com>
2021-02-07 00:04:29 +0200

    bump version to 1.1.0
    added the "-qq" tag to add-apt-repository & apt-get (was missing)
    made the install complete message more concise
    removed the second install complete message, because it was redundant

--- resources/customizations/system76-wallpapers.sh
+++ resources/customizations/system76-wallpapers.sh
@@ -1,12 +1,11 @@
 # system76-wallpapers
-# version 1.0.1
+# version 1.1.0
 
 echo -e "\e[1;45mAdding System76 Wallpapers! \e[0m"
 
-sudo apt-add-repository -ys ppa:system76-dev/stable
-sudo apt-get update
+sudo apt-add-repository -qqy -s ppa:system76-dev/stable
+sudo apt-get update -qq
 
 sudo apt install -qqy system76-wallpapers
 
-echo -e "\e[1;32msystem76-wallpapers have been added to /usr/share/backgrounds \e[0m"
-echo -e "\e[1;32mSystem76 Wallpapers have been added! \e[0m"
+echo -e "\e[1;32mThe wallpapers from system76-wallpapers have been added to /usr/share/backgrounds \e[0m"
