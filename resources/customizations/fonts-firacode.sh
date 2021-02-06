bcd626c80bdf8511fe1371d19f792789b5e8591d
thefirethirteen <78559591+thefirethirteen@users.noreply.github.com>
2021-02-06 23:18:52 +0200

    bump version to 1.0.1
    added -qq flag to "apt-get install" (was missing)
    added -y flag to "apt-get install" (was missing)

--- resources/customizations/fonts-firacode.sh
+++ resources/customizations/fonts-firacode.sh
@@ -1,8 +1,8 @@
 # fonts-firacode.sh
-# version 1.0.0
+# version 1.0.1
 
 echo -e "\e[1;45mAdding fonts-firacode! \e[0m"
 
-sudo apt-get install fonts-firacode
+sudo apt-get install -qqy fonts-firacode
 
 echo -e "\e[1;32mfonts-firacode has been added! \e[0m"
