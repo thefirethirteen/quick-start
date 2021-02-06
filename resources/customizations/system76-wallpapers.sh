343ea1a38a0cb5200f020fddff8af3ed343914ad
thefirethirteen <78559591+thefirethirteen@users.noreply.github.com>
2021-02-07 00:15:02 +0200

    bump version to 1.1.1

--- resources/customizations/system76-wallpapers.sh
+++ resources/customizations/system76-wallpapers.sh
@@ -1,11 +1,11 @@
 # system76-wallpapers
-# version 1.1.0
+# version 1.1.1
 
 echo -e "\e[1;45mAdding System76 Wallpapers! \e[0m"
 
-sudo apt-add-repository -qqy -s ppa:system76-dev/stable
-sudo apt-get update -qq
+sudo apt-add-repository -qq -y -s ppa:system76-dev/stable
+sudo apt-get -qq update
 
-sudo apt install -qqy system76-wallpapers
+sudo apt-get -qq -y install system76-wallpapers
 
 echo -e "\e[1;32mThe wallpapers from system76-wallpapers have been added to /usr/share/backgrounds \e[0m"
