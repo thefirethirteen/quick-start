3464ec4d98a97c7cc0ef49cc68465b259a67ff87
thefirethirteen <78559591+thefirethirteen@users.noreply.github.com>
2021-02-06 23:58:40 +0200

    bump version to 1.0.3
    added the "-qq" tag to apt-key, add-apt-repository and apt-get (was missing)

--- resources/apps/miscellaneous/wine.sh
+++ resources/apps/miscellaneous/wine.sh
@@ -1,16 +1,16 @@
 # wine.sh
-# version 1.0.2
+# version 1.0.3
 
 echo -e "\e[1;45mInstalling wine! \e[0m"
 
 sudo dpkg --add-architecture i386
 
 wget -nc https://dl.winehq.org/wine-builds/winehq.key
-sudo apt-key add winehq.key
+sudo apt-key -qq add winehq.key
 rm -f winehq.key
 
-sudo add-apt-repository -y 'deb https://dl.winehq.org/wine-builds/ubuntu/ groovy main'
-sudo apt-get update
+sudo add-apt-repository -qqy 'deb https://dl.winehq.org/wine-builds/ubuntu/ groovy main'
+sudo apt-get update -qq
 
 sudo apt install -qqy --install-recommends winehq-stable
 
