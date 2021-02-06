279b2fc9d4d7552a433935bc4a5bc491f5d35157
thefirethirteen <78559591+thefirethirteen@users.noreply.github.com>
2021-02-07 00:40:34 +0200

    bump version to 1.0.4
    remove "-qq" from apt-add-repository; incompatible with apt-add-repository

--- resources/apps/development/codeblocks.sh
+++ resources/apps/development/codeblocks.sh
@@ -1,13 +1,13 @@
 # codeblocks.sh
-# version 1.0.3
+# version 1.0.4
 
 echo -e "\e[1;45mInstalling Code::Blocks! \e[0m"
 
-sudo add-apt-repository -qq -y ppa:codeblocks-devs/release
+sudo add-apt-repository -y ppa:codeblocks-devs/release
 sudo apt-get -qq -y update
 
 sudo apt-get -qq -y install codeblocks codeblocks-contrib
 
-sudo add-apt-repository -qq -y -r ppa:codeblocks-devs/release
+sudo add-apt-repository -y -r ppa:codeblocks-devs/release
 
 echo -e "\e[1;32mCode::Blocks has been installed! \e[0m"
