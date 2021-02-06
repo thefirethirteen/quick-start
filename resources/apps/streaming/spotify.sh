99f6da90973c73a5c16e05a7cac62e1a91ffab0c
thefirethirteen <78559591+thefirethirteen@users.noreply.github.com>
2021-02-07 00:15:39 +0200

    bump version to 1.1.1

--- resources/apps/streaming/spotify.sh
+++ resources/apps/streaming/spotify.sh
@@ -1,5 +1,5 @@
 # spotify.sh
-# version 1.1.0
+# version 1.1.1
 
 echo -e "\e[1;45mInstalling Spotify! \e[0m"
 
@@ -7,8 +7,8 @@ curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg
 sudo apt-key -qq add -
 sudo tee /etc/apt/sources.list.d/spotify.list
 
-sudo apt-get update -qq
+sudo apt-get -qq update
 
-sudo apt-get install -qqy spotify-client
+sudo apt-get -qq -y install spotify-client
 
 echo -e "\e[1;32mSpotify has been installed! \e[0m"
