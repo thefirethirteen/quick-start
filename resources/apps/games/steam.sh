c83606a49273904b168c30a53eb2c1e82ae0c76e
thefirethirteen <78559591+thefirethirteen@users.noreply.github.com>
2021-02-07 00:16:36 +0200

    bump version to 1.0.2

--- resources/apps/games/steam.sh
+++ resources/apps/games/steam.sh
@@ -1,8 +1,8 @@
 # steam.sh
-# version 1.0.1
+# version 1.0.2
 
 echo -e "\e[1;45mInstalling Steam! \e[0m"
 
-sudo apt-get -qqy install steam
+sudo apt-get -qq -y install steam
 
 echo -e "\e[1;32mSteam has been installed! \e[0m"
