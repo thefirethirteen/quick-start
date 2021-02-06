3c88e234571b13ddff38ed1017246ae0119d415c
thefirethirteen <78559591+thefirethirteen@users.noreply.github.com>
2021-02-07 00:50:28 +0200

    bump version to 1.0.3

--- resources/apps/social/discord.sh
+++ resources/apps/social/discord.sh
@@ -1,8 +1,8 @@
 # discord.sh
-# version 1.0.2
+# version 1.0.3
 
 echo -e "\e[1;45mInstalling discord! \e[0m"
 
-sudo apt -qq -y install discord
+sudo apt-get -qq -y install discord
 
 echo -e "\e[1;32mDiscord has been installed! \e[0m"
