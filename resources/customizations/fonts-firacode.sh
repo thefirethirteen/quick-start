8d08e629a1e6520329e13f1c61ec61b159ce4cad
thefirethirteen <78559591+thefirethirteen@users.noreply.github.com>
2021-02-07 00:15:33 +0200

    bump version to 1.0.2

--- resources/customizations/fonts-firacode.sh
+++ resources/customizations/fonts-firacode.sh
@@ -1,8 +1,8 @@
 # fonts-firacode.sh
-# version 1.0.1
+# version 1.0.2
 
 echo -e "\e[1;45mAdding fonts-firacode! \e[0m"
 
-sudo apt-get install -qqy fonts-firacode
+sudo apt-get -qq -y install fonts-firacode
 
 echo -e "\e[1;32mfonts-firacode has been added! \e[0m"
