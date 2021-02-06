fbb8d3bf6a6caaa1f83ec1d290158a56b99b3963
thefirethirteen <78559591+thefirethirteen@users.noreply.github.com>
2021-02-07 00:14:31 +0200

    bump version to 1.0.3

--- resources/apps/development/codeblocks.sh
+++ resources/apps/development/codeblocks.sh
@@ -1,13 +1,13 @@
 # codeblocks.sh
-# version 1.0.2
+# version 1.0.3
 
 echo -e "\e[1;45mInstalling Code::Blocks! \e[0m"
 
-sudo add-apt-repository -qqy ppa:codeblocks-devs/release
-sudo apt-get update
+sudo add-apt-repository -qq -y ppa:codeblocks-devs/release
+sudo apt-get -qq -y update
 
-sudo apt-get install -qqy codeblocks codeblocks-contrib
+sudo apt-get -qq -y install codeblocks codeblocks-contrib
 
-sudo add-apt-repository -qqy -r ppa:codeblocks-devs/release
+sudo add-apt-repository -qq -y -r ppa:codeblocks-devs/release
 
 echo -e "\e[1;32mCode::Blocks has been installed! \e[0m"
