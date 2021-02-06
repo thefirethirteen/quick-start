5b682eef6c77c4b82cb03d3d7075c600b314bab9
thefirethirteen <78559591+thefirethirteen@users.noreply.github.com>
2021-02-06 23:19:10 +0200

    bump version to 1.0.1
    added -qq flag to "apt-get install" (was missing)

--- resources/customizations/system76-wallpapers.sh
+++ resources/customizations/system76-wallpapers.sh
@@ -1,12 +1,12 @@
 # system76-wallpapers
-# version 1.0.0
+# version 1.0.1
 
 echo -e "\e[1;45mAdding System76 Wallpapers! \e[0m"
 
 sudo apt-add-repository -ys ppa:system76-dev/stable
 sudo apt-get update
 
-sudo apt install -y system76-wallpapers
+sudo apt install -qqy system76-wallpapers
 
 echo -e "\e[1;32msystem76-wallpapers have been added to /usr/share/backgrounds \e[0m"
 echo -e "\e[1;32mSystem76 Wallpapers have been added! \e[0m"
