f5baae5b19d84fcc4b5f9d17efc1a7aeca8843ea
thefirethirteen <78559591+thefirethirteen@users.noreply.github.com>
2021-02-18 00:33:31 +0200

    bump version to 1.4.0

--- resources/main.py
+++ resources/main.py
@@ -1,53 +1,45 @@
 # main.py
-# version 1.1.0
+# version 1.4.0
 
 import os
 
 os.chdir("main")
 
-print("This script will install desktop environments, apps, customizations and copy app configs.")
-print("Do you want to continue? [Y/n]")
+#update package database
+os.system("sudo apt-get update")
 
+#add required repositories
+os.system('sudo add-apt-repository -y "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"')
+
+#upgrade packages
+print("Do you want to install all package updates available? [Y/n]")
 USER_INPUT = input()
-
 if USER_INPUT == "y":
-    #update package database
-    os.system("sudo apt-get update")
+    os.system("sudo apt-get -y upgrade --allow-downgrades")
 
-    #required repositories
-    os.system('sudo add-apt-repository -y "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"')
+#runtimes.py
+print("Do you want to install any runtimes? [Y/n]")
+USER_INPUT = input()
+if USER_INPUT == "y":
+    os.system("python3 runtimes.py")
 
-    #package update
-    print("Do you want to install all package updates available? [Y/n]")
-    USER_INPUT = input()
-    if USER_INPUT == "y":
-        os.system("sudo apt-get -y upgrade --allow-downgrades")
+#de.py
+print("Do you want to install any desktop environments? [y/N]")
+USER_INPUT = input()
+if USER_INPUT == "y":
+    os.system("python3 de.py")
 
-    #runtimes.sh
-    print("Do you want to install any runtimes? [Y/n]")
-    USER_INPUT = input()
-    if USER_INPUT == "y":
-        os.system("bash runtimes.sh")
+#apps.py
+print("Do you want to install any apps? [Y/n]")
+USER_INPUT = input()
+if USER_INPUT == "y":
+    os.system("python3 apps.py")
 
-    #de.sh
-    print("Do you want to install any desktop environments? [y/N]")
-    USER_INPUT = input()
-    if USER_INPUT == "y":
-        os.system("bash de.sh")
+#customizations.py
+print("Do you want to install any customizations? [Y/n]")
+USER_INPUT = input()
+if USER_INPUT == "y":
+    os.system("python3 customizations.py")
 
-    #apps.sh
-    print("Do you want to install any apps? [Y/n]")
-    USER_INPUT = input()
-    if USER_INPUT == "y":
-        os.system("python apps.py")
-
-    #customizations.sh
-    print("Do you want to install any customizations? [Y/n]")
-    USER_INPUT = input()
-    if USER_INPUT == "y":
-        os.system("bash customizations.sh")
-
-    #remove unnecessary packages
-    os.system("sudo apt-get -y autoremove")
-
-#echo -e "\e[1;33mAfter restarting, start [next_script_name].sh using start.sh  \e[0m"
+#remove unnecessary packages
+os.system("sudo apt-get -y autoremove")
