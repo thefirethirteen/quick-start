cf95aa3b18356e226561632b1c9ee9ba16dbd47f
thefirethirteen <78559591+thefirethirteen@users.noreply.github.com>
2021-02-17 15:34:13 +0200

    bump version to 1.1.0
    added support for installing gnome

--- resources/main/de.sh
+++ resources/main/de.sh
@@ -1,12 +1,20 @@
 # de.sh
-# version 1.0.0
+# version 1.1.0
 
 cd de
 
 #mate.sh
-echo -e "\e[1;33mWould you like to install the Mate Desktop Environment? y/n \e[0m"
+echo -e "\e[1;33mWould you like to install the Mate Desktop Environment? [Y/n] \e[0m"
 read USER_INPUT
 if [ "$USER_INPUT" == "y" ]
 then
     bash mate.sh
+fi
+
+#gnome.sh
+echo -e "\e[1;33mWould you like to install the Gnome Desktop Environment? [Y/n] \e[0m"
+read USER_INPUT
+if [ "$USER_INPUT" == "y" ]
+then
+    bash gnome.sh
 fi
