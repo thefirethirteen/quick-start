fc328fbb333813158c8117e3480dfd308c129c1d
thefirethirteen <78559591+thefirethirteen@users.noreply.github.com>
2021-02-17 15:34:47 +0200

    bump version to 1.0.3

--- resources/main/de/mate.sh
+++ resources/main/de/mate.sh
@@ -1,8 +1,8 @@
 # mate.sh
-# version 1.0.2
+# version 1.0.3
 
 echo -e "\e[1;45mInstalling the Mate Desktop Environment \e[0m"
 
-sudo apt-get -qq -y install mate-desktop-environment mate-desktop-environment-extras ubuntu-mate-themes
+sudo apt-get -y install mate-desktop-environment mate-desktop-environment-extras ubuntu-mate-themes
 
 echo -e "\e[1;32mThe Mate Desktop Environment has been installed! \e[0m"
