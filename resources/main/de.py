77694fe35b72e57f4ca0fb30ff53c926963387e4
thefirethirteen <78559591+thefirethirteen@users.noreply.github.com>
2021-02-18 00:37:46 +0200

    bump version to 1.0.2

--- resources/main/de.py
+++ resources/main/de.py
@@ -1,5 +1,5 @@
 # de.py
-# version 1.0.1
+# version 1.0.2
 
 import os
 
@@ -8,11 +8,11 @@ os.chdir("de")
 #mate.sh
 print("Do you want to install the Mate Desktop Environment? [y/n]")
 USER_INPUT = input()
-if USER_INPUT" == "y":
+if USER_INPUT == "y":
     os.system("bash mate.sh")
 
 #gnome.sh
 echo -e "Do you want to install the Gnome Desktop Environment? [y/n]"
 USER_INPUT = input()
-if USER_INPUT" == "y":
+if USER_INPUT == "y":
     os.system("bash gnome.sh")
