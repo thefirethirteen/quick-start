6442a39869ed6d04a68a5538b04dff21073b4698
thefirethirteen <78559591+thefirethirteen@users.noreply.github.com>
2021-02-18 00:34:16 +0200

    create runtimes.py

---
+++ resources/main/runtimes.py
@@ -0,0 +1,12 @@
+# runtimes.py
+# version 1.0.0
+
+import os
+
+os.chdir("runtimes")
+
+#java.sh
+print("Do you want to install java? [Y/n]")
+USER_INPUT = input()
+if USER_INPUT" == "y":
+    os.system("bash java.sh")
