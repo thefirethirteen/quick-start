20f25b53d81505ec428603604d75ed045c0e6bd3
thefirethirteen <78559591+thefirethirteen@users.noreply.github.com>
2021-02-18 00:34:27 +0200

    create de.py

---
+++ resources/main/de.py
@@ -0,0 +1,18 @@
+# de.py
+# version 1.0.1
+
+import os
+
+os.chdir("de")
+
+#mate.sh
+print("Do you want to install the Mate Desktop Environment? [y/n]")
+USER_INPUT = input()
+if USER_INPUT" == "y":
+    os.system("bash mate.sh")
+
+#gnome.sh
+echo -e "Do you want to install the Gnome Desktop Environment? [y/n]"
+USER_INPUT = input()
+if USER_INPUT" == "y":
+    os.system("bash gnome.sh")
