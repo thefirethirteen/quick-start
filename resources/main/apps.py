ca60320eaed776b3c9e636e7703c644bd9cec528
thefirethirteen <78559591+thefirethirteen@users.noreply.github.com>
2021-02-17 22:47:26 +0200

    create apps.py

---
+++ resources/main/apps.py
@@ -0,0 +1,42 @@
+# apps.py
+# version 1.0.0
+
+import os
+
+os.chdir("apps")
+
+#office.sh
+print("Do you want to install any office suites? [Y/n]")
+USER_INPUT = input()
+if USER_INPUT == "y":
+    os.system("bash office.sh")
+
+#social.sh
+print("Do you want to install any social apps? [Y/n]")
+USER_INPUT = input()
+if USER_INPUT" == "y":
+    os.system("bash social.sh")
+
+#development.sh
+print("Do you want to install any development apps? [Y/n]")
+USER_INPUT = input()
+if USER_INPUT" == "y":
+    os.system("bash development.sh")
+
+#streaming.sh
+print("Do you want to install any streaming apps? [Y/n]")
+USER_INPUT = input()
+if USER_INPUT" == "y":
+    os.system("bash streaming.sh")
+
+#games.sh
+print("Do you want to install any games? [Y/n]")
+USER_INPUT = input()
+if USER_INPUT" == "y":
+    os.system("bash games.sh")
+
+#miscellaneous.sh
+print("Do you want to install any miscellaneous apps? [Y/n]")
+USER_INPUT = input()
+if USER_INPUT" == "y":
+    os.system("bash miscellaneous.sh")
