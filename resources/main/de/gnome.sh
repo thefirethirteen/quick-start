dc2552df3147cde3f1d55ad887dd48b537aa9a48
thefirethirteen <78559591+thefirethirteen@users.noreply.github.com>
2021-02-17 15:34:24 +0200

    Create gnome.sh
    installs gnome

---
+++ resources/main/de/gnome.sh
@@ -0,0 +1,8 @@
+# gnome.sh
+# version 1.0.0
+
+echo -e "\e[1;45mInstalling the Gnome Desktop Environment \e[0m"
+
+sudo apt-get -y install gnome-session
+
+echo -e "\e[1;32mThe Gnome Desktop Environment has been installed! \e[0m"
