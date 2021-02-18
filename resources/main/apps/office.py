f4ea8068d9e12954649569756e1c27084ec80cdf
thefirethirteen <78559591+thefirethirteen@users.noreply.github.com>
2021-02-18 22:29:32 +0200

    create office.py

---
+++ resources/main/apps/office.py
@@ -0,0 +1,13 @@
+# office.py
+# version 1.0.0
+
+import subprocess
+import os
+
+os.chdir("office")
+
+#openoffice.sh
+print("Do you want to install Apache OpenOffice? [y/n]")
+USER_INPUT = input()
+if USER_INPUT == "y":
+    subprocess.run(["bash", "openoffice.sh"])
