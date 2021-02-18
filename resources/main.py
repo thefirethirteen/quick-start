26f928ed454d5153146c963f9ac38e311805b5f6
thefirethirteen <florea13andrew@gmail.com>
2021-02-18 19:16:31 +0200

    bump version to 2.0.0

--- resources/main.py
+++ resources/main.py
@@ -1,45 +1,46 @@
 # main.py
-# version 1.4.0
+# version 2.0.0
 
+import subprocess
 import os
 
 os.chdir("main")
 
 #update package database
-os.system("sudo apt-get update")
+subprocess.run(["sudo", "apt-get", "update"])
 
 #add required repositories
-os.system('sudo add-apt-repository -y "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"')
+subprocess.run(["sudo", "add-apt-repository", "-y", "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"])
 
 #upgrade packages
 print("Do you want to install all package updates available? [Y/n]")
 USER_INPUT = input()
 if USER_INPUT == "y":
-    os.system("sudo apt-get -y upgrade --allow-downgrades")
+    subprocess.run(["sudo", "apt-get", "-y", "upgrade"])
 
 #runtimes.py
 print("Do you want to install any runtimes? [Y/n]")
 USER_INPUT = input()
 if USER_INPUT == "y":
-    os.system("python3 runtimes.py")
+    subprocess.run(["python3", "runtimes.py"])
 
 #de.py
 print("Do you want to install any desktop environments? [y/N]")
 USER_INPUT = input()
 if USER_INPUT == "y":
-    os.system("python3 de.py")
+    subprocess.run(["python3", "de.py"])
 
 #apps.py
 print("Do you want to install any apps? [Y/n]")
 USER_INPUT = input()
 if USER_INPUT == "y":
-    os.system("python3 apps.py")
+    subprocess.run(["python3", "apps.py"])
 
 #customizations.py
 print("Do you want to install any customizations? [Y/n]")
 USER_INPUT = input()
 if USER_INPUT == "y":
-    os.system("python3 customizations.py")
+    subprocess.run(["python3", "customizations.py"])
 
 #remove unnecessary packages
-os.system("sudo apt-get -y autoremove")
+subprocess.run(["sudo", "apt-get", "-y", "autoremove"])
