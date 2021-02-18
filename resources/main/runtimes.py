da9d607c5c0d2419ac9ae40f3ed5b094f05148b9
thefirethirteen <florea13andrew@gmail.com>
2021-02-18 19:34:05 +0200

    bump version to 2.1.0

--- resources/main/runtimes.py
+++ resources/main/runtimes.py
@@ -1,5 +1,5 @@
 # runtimes.py
-# version 2.0.0
+# version 2.1.0
 
 import subprocess
 import os
@@ -10,4 +10,4 @@ os.chdir("runtimes")
 print("Do you want to install java? [Y/n]")
 USER_INPUT = input()
 if USER_INPUT == "y":
-    subprocess.run(["bash", "java.sh"])
+    subprocess.run(["python3", "java.py"])
