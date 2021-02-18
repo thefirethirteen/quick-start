6c9f305561f94632c3031eb21efd870572d55f93
thefirethirteen <florea13andrew@gmail.com>
2021-02-18 19:34:18 +0200

    remove java.sh

--- resources/main/runtimes/java.sh
+++
@@ -1,8 +0,0 @@
-# java.sh
-# version 1.1.2
-
-echo -e "\e[1;45mInstalling the latest OpenJDK! \e[0m"
-
-sudo apt-get -qq -y install default-jdk
-
-echo -e "\e[1;32mOpenJDK has been installed! \e[0m"
