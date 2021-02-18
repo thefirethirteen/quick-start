44c8e05896e98fdd3763c7edb414a66daa1c707d
thefirethirteen <florea13andrew@gmail.com>
2021-02-18 21:07:11 +0200

    remove fonts-firacode.sh

--- resources/main/customizations/fonts-firacode.sh
+++
@@ -1,8 +0,0 @@
-# fonts-firacode.sh
-# version 1.0.3
-
-echo -e "\e[1;45mAdding firacode fonts! \e[0m"
-
-sudo apt-get -qq -y install fonts-firacode
-
-echo -e "\e[1;32mfiracode fonts have been added! \e[0m"
