2d8925d75b68b336d4e4035e115fc7b7f646eabb
thefirethirteen <florea13andrew@gmail.com>
2021-02-18 19:39:29 +0200

    remove mate.sh

--- resources/main/de/mate.sh
+++
@@ -1,8 +0,0 @@
-# mate.sh
-# version 1.0.3
-
-echo -e "\e[1;45mInstalling the Mate Desktop Environment \e[0m"
-
-sudo apt-get -y install mate-desktop-environment mate-desktop-environment-extras ubuntu-mate-themes
-
-echo -e "\e[1;32mThe Mate Desktop Environment has been installed! \e[0m"
