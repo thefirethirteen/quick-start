afe4cb386c4b26de3f3a37b3d4a0b18b297c38b5
thefirethirteen <florea13andrew@gmail.com>
2021-02-18 21:03:50 +0200

    bump version to 2.1.0

--- start.sh
+++ start.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 # start.sh
-# version 2.0.1
+# version 2.1.0
 
 cd resources
-
-#prerequisite for required packages
-sudo apt-get update
-
-#required packages
-sudo apt-get -y install python3
 
 python3 main.py
 
