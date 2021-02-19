b2f89ea28b4afd3d5d79e7a02be43e23c29aa0cf
thefirethirteen <78559591+thefirethirteen@users.noreply.github.com>
2021-02-19 21:39:36 +0200

    bump version to 1.1.2

--- resources/main/apps/miscellaneous/wine.sh
+++ resources/main/apps/miscellaneous/wine.sh
@@ -1,17 +1,13 @@
 # wine.sh
-# version 1.1.1
-
-echo -e "\e[1;45mInstalling wine! \e[0m"
+# version 1.1.2
 
 sudo dpkg --add-architecture i386
 
 wget -nc https://dl.winehq.org/wine-builds/winehq.key
-sudo apt-key -qq add winehq.key
+sudo apt-key add winehq.key
 rm -f winehq.key
 
 sudo add-apt-repository -y 'deb https://dl.winehq.org/wine-builds/ubuntu/ groovy main'
-sudo apt-get -qq update
+sudo apt-get update
 
-sudo apt-get -qq -y install --install-recommends winehq-stable
-
-echo -e "\e[1;32mwinehq-stable has been installed to /opt/wine-stable \e[0m"
+sudo apt-get -y install --install-recommends winehq-stable
