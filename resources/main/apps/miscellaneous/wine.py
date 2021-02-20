111be299adc451a47f5cfbabdb05c54b43026ef5
thefirethirteen <florea13andrew@gmail.com>
2021-02-20 10:01:43 +0200

    bump version to 1.0.2

--- resources/main/apps/miscellaneous/wine.py
+++ resources/main/apps/miscellaneous/wine.py
@@ -1,8 +1,7 @@
 # wine.py
-# version 1.0.1
+# version 1.0.2
 
 import subprocess
-#import os
 
 subprocess.run(["sudo", "dpkg", "--add-architecture", "i386"])
 
@@ -11,7 +10,6 @@ subprocess.run(["sudo", "apt-key", "add", "winehq.key"])
 subprocess.run(["rm", "-f", "winehq.key"])
 
 subprocess.run(["sudo", "add-apt-repository", "-y", "deb https://dl.winehq.org/wine-builds/ubuntu/ groovy main"])
-#os.system("sudo add-apt-repository -y 'deb https://dl.winehq.org/wine-builds/ubuntu/ groovy main'")
 subprocess.run(["sudo", "apt-get", "update"])
 
 subprocess.run(["sudo", "apt-get", "-y", "install", "--install-recommends", "winehq-stable"])
