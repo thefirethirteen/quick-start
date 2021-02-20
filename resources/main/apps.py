0c15a27b3c329b9e30d126d57ae63970544eefe8
thefirethirteen <florea13andrew@gmail.com>
2021-02-20 11:48:18 +0200

    bump version to 2.7.1

--- resources/main/apps.py
+++ resources/main/apps.py
@@ -1,10 +1,16 @@
 # apps.py
-# version 2.7.0
+# version 2.7.1
 
 import subprocess
 import os
 
 os.chdir("apps")
+
+#browser.py
+print("Do you want to install any browsers? [Y/n]")
+USER_INPUT = input()
+if USER_INPUT == "y":
+    subprocess.run(["python3", "browser.py"])
 
 #office.py
 print("Do you want to install any office suites? [Y/n]")
@@ -18,12 +24,6 @@ USER_INPUT = input()
 if USER_INPUT == "y":
     subprocess.run(["python3", "social.py"])
 
-#development.py
-print("Do you want to install any development apps? [Y/n]")
-USER_INPUT = input()
-if USER_INPUT == "y":
-    subprocess.run(["python3", "development.py"])
-
 #streaming.py
 print("Do you want to install any streaming apps? [Y/n]")
 USER_INPUT = input()
@@ -36,14 +36,14 @@ USER_INPUT = input()
 if USER_INPUT == "y":
     subprocess.run(["python3", "games.py"])
 
+#development.py
+print("Do you want to install any development apps? [Y/n]")
+USER_INPUT = input()
+if USER_INPUT == "y":
+    subprocess.run(["python3", "development.py"])
+
 #miscellaneous.py
 print("Do you want to install any miscellaneous apps? [Y/n]")
 USER_INPUT = input()
 if USER_INPUT == "y":
     subprocess.run(["python3", "miscellaneous.py"])
-
-#browser.py
-print("Do you want to install any browsers? [Y/n]")
-USER_INPUT = input()
-if USER_INPUT == "y":
-    subprocess.run(["python3", "browser.py"])
