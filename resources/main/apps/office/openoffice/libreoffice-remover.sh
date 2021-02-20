b64e269e5d138241bf08ef85f24f65d13dc89d9e
thefirethirteen <florea13andrew@gmail.com>
2021-02-20 10:59:26 +0200

    remove libreoffice-remover.sh

--- resources/main/apps/office/openoffice/libreoffice-remover.sh
+++
@@ -1,9 +0,0 @@
-# libreoffice-remover.sh
-# version 1.0.1
-
-echo -e "\e[1;45mRemoving LibreOffice! \e[0m"
-
-sudo apt-get -qq -y remove "libreoffice*"
-sudo apt-get -qq -y autoremove
-
-echo -e "\e[1;32mLibreOffice has been removed! \e[0m"
