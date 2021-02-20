b0f7aeb3bb7f3190d41593faf0310ef8609b9973
thefirethirteen <florea13andrew@gmail.com>
2021-02-20 11:48:35 +0200

    bump version to 2.1.1

--- resources/main/de.py
+++ resources/main/de.py
@@ -1,19 +1,19 @@
 # de.py
-# version 2.1.0
+# version 2.1.1
 
 import subprocess
 import os
 
 os.chdir("de")
 
-#mate.sh
-print("Do you want to install the Mate Desktop Environment? [y/n]")
-USER_INPUT = input()
-if USER_INPUT == "y":
-    subprocess.run(["python3", "mate.py"])
-
-#gnome.sh
+#gnome.py
 print("Do you want to install the Gnome Desktop Environment? [y/n]")
 USER_INPUT = input()
 if USER_INPUT == "y":
     subprocess.run(["python3", "gnome.py"])
+
+#mate.py
+print("Do you want to install the Mate Desktop Environment? [y/n]")
+USER_INPUT = input()
+if USER_INPUT == "y":
+    subprocess.run(["python3", "mate.py"])
