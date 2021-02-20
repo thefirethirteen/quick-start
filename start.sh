b5744ca83fd5e0b633679e58143f372672a4c04f
thefirethirteen <florea13andrew@gmail.com>
2021-02-20 19:14:26 +0200

    remove commented-out code

--- start.sh
+++ start.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
 # start.sh
-# version 2.1.0
+# version 2.1.1
 
 cd resources
 
 python3 main.py
-
-#echo "Which script would you like to start?"
-
-#echo "Available scripts: main.sh"
-
-#echo "(If unsure, type main.sh)"
-
-#read USER_INPUT
-#bash "$USER_INPUT"
