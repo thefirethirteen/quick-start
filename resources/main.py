7e0d1a7ab118fa1584622af80eb702bded97d490
thefirethirteen <florea13andrew@gmail.com>
2021-02-20 10:37:29 +0200

    bump version to 2.0.2

--- resources/main.py
+++ resources/main.py
@@ -1,25 +1,19 @@
 # main.py
-# version 2.0.1
+# version 2.0.2
 
 import subprocess
 import os
 
 os.chdir("main")
 
+#add required repositories
+subprocess.run(["sudo", "add-apt-repository", "-y", "--no-update", "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"])
+
 #update package database
 subprocess.run(["sudo", "apt-get", "update"])
 
-#add required repositories
-subprocess.run(["sudo", "add-apt-repository", "-y", "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"])
-
 #install required packages
 subprocess.run(["sudo", "apt-get", "-y", "install", "curl"])
-
-#upgrade packages
-print("Do you want to install all package updates available? [Y/n]")
-USER_INPUT = input()
-if USER_INPUT == "y":
-    subprocess.run(["sudo", "apt-get", "-y", "upgrade"])
 
 #runtimes.py
 print("Do you want to install any runtimes? [Y/n]")
@@ -44,6 +38,12 @@ print("Do you want to install any customizations? [Y/n]")
 USER_INPUT = input()
 if USER_INPUT == "y":
     subprocess.run(["python3", "customizations.py"])
+
+#upgrade packages
+print("Do you want to install all package updates available? [Y/n]")
+USER_INPUT = input()
+if USER_INPUT == "y":
+    subprocess.run(["sudo", "apt-get", "-y", "upgrade"])
 
 #remove unnecessary packages
 subprocess.run(["sudo", "apt-get", "-y", "autoremove"])
