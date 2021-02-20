f0cc9e80094ee8f913f7463bf838d1ec64a155f9
thefirethirteen <florea13andrew@gmail.com>
2021-02-20 19:24:17 +0200

    bump version to 2.4.0

--- resources/main/customizations.py
+++ resources/main/customizations.py
@@ -1,25 +1,25 @@
 # customizations.py
-# version 2.3.1
+# version 2.4.0
 
 import subprocess
 import os
 
 os.chdir("customizations")
 
-#systen76-wallpapers.py
-print("Do you want to add system76 wallpapers? [y/n]")
+#wallpapers.py
+print("Do you want to add any wallpapers? [y/n]")
 USER_INPUT = input()
 if USER_INPUT == "y":
-    subprocess.run(["python3", "system76-wallpapers.py"])
+    subprocess.run(["python3", "wallpapers.py"])
 
-#iconpack-obsidian.py
-print("Do you want to add iconpack-obsidian? [y/n]")
+#icons.py
+print("Do you want to add any icons? [y/n]")
 USER_INPUT = input()
 if USER_INPUT == "y":
-    subprocess.run(["python3", "iconpack-obsidian.py"])
+    subprocess.run(["python3", "icons.py"])
 
-#fonts-firacode.py
-print("Do you want to add firacode fonts? [Y/n]")
+#fonts.py
+print("Do you want to add any fonts? [y/n]")
 USER_INPUT = input()
 if USER_INPUT == "y":
-    subprocess.run(["python3", "fonts-firacode.py"])
+    subprocess.run(["python3", "fonts.py"])
