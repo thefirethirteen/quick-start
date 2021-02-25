e7977c76fe4d8cbcc0ea50cf11a59cc6272d25e6
thefirethirteen <florea13andrew@gmail.com>
2021-02-26 00:06:51 +0200

    create codeblocks.py

---
+++ resources/apps/development/codeblocks.py
@@ -0,0 +1,26 @@
+# quick-start - a collection of scripts for a quick start
+# Copyright (C) 2021 Andrew F
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# codeblocks.py
+
+import subprocess
+
+subprocess.run(["sudo", "add-apt-repository", "-y", "ppa:codeblocks-devs/release"])
+subprocess.run(["sudo", "apt-get", "update"])
+
+subprocess.run(["sudo", "apt-get", "-y", "install", "codeblocks", "codeblocks-contrib"])
+
+subprocess.run(["sudo", "add-apt-repository", "-y", "-r", "ppa:codeblocks-devs/release"])
