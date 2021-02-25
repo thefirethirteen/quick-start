0e0e7a2908e4e4a2368a9c792eeb3d0173bfd250
thefirethirteen <florea13andrew@gmail.com>
2021-02-25 23:38:33 +0200

    update streaming.py

--- resources/apps/streaming.py
+++ resources/apps/streaming.py
@@ -1,28 +1,27 @@
-#quick-start - a collection of scripts for a quick start
-#Copyright (C) 2021 Andrew F
+# quick-start - a collection of scripts for a quick start
+# Copyright (C) 2021 Andrew F
 
-#This program is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
-#You should have received a copy of the GNU General Public License
-#along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 # streaming.py
-# version 1.2.0
 
 import subprocess
 import os
 
 os.chdir("streaming")
 
-#spotify.py
+# spotify.py
 print("Do you want to install Spotify? [y/n]")
 USER_INPUT = input()
 if USER_INPUT == "y":
