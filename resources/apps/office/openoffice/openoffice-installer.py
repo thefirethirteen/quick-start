f64ae3bbfe6702374726ed5e73be07186f1619c4
thefirethirteen <florea13andrew@gmail.com>
2021-02-25 23:40:35 +0200

    update openoffice-installer.py

--- resources/apps/office/openoffice/openoffice-installer.py
+++ resources/apps/office/openoffice/openoffice-installer.py
@@ -1,21 +1,20 @@
-#quick-start - a collection of scripts for a quick start
-#Copyright (C) 2021 Andrew F
+# quick-start - a collection of scripts for a quick start
+# Copyright (C) 2021 Andrew F
 
-#This program is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
-#You should have received a copy of the GNU General Public License
-#along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 # openoffice-installer.py
-# version 1.1.0
 
 import subprocess
 import os
@@ -27,13 +26,19 @@ VERSION = input()
 
 subprocess.run(["wget", "-O", "openoffice-download", "https://sourceforge.net/projects/openofficeorg.mirror/files/" + VERSION + "/binaries/en-GB/Apache_OpenOffice_" + VERSION + "_Linux_x86-64_install-deb_en-GB.tar.gz/download"])
 subprocess.run(["tar", "-xf", "openoffice-download"])
+
 os.chdir("en-GB")
 os.chdir("DEBS")
+
 os.system("sudo dpkg -i *.deb")
+
 os.chdir("desktop-integration")
+
 os.system("sudo dpkg -i *.deb")
+
 os.chdir("..")
 os.chdir("..")
 os.chdir("..")
+
 subprocess.run(["rm", "-f", "openoffice-download"])
 subprocess.run(["rm", "-r", "-f", "en-GB"])
