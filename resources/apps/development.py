34785b3d331172d0fe6fe0dc687731b4307fce41
thefirethirteen <florea13andrew@gmail.com>
2021-02-26 00:08:35 +0200

    update development.py

--- resources/apps/development.py
+++ resources/apps/development.py
@@ -17,9 +17,20 @@
 # development.py
 
 import subprocess
+import os
 
-# import os
+os.chdir("development")
 
-# os.chdir("development")
+# codeblocks.py
+print("Do you want to install Code::Blocks? [y/n]")
+USER_INPUT = input()
 
-print("No development apps available!")
+if USER_INPUT == "y":
+    subprocess.run(["python3", "codeblocks.py"])
+
+# jetbrains-toolbox.py
+print("Do you want to install Steam? [y/n]")
+USER_INPUT = input()
+
+if USER_INPUT == "y":
+    subprocess.run(["python3", "jetbrains-toolbox.py"])
