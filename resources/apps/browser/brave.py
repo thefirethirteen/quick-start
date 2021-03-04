d05d3f7e069d4dfc21dd1b6063fc227cc87cf3bf
thefirethirteen <florea13andrew@gmail.com>
2021-03-04 21:46:53 +0200

    create brave.py

---
+++ resources/apps/browser/brave.py
@@ -0,0 +1,27 @@
+# quick-start - a collection of scripts for a quick start
+# Copyright (C) 2021 Andrew F
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# brave.py
+
+import subprocess
+import os
+
+os.system("curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -")
+os.system("echo 'deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main' | sudo tee /etc/apt/sources.list.d/brave-browser-release.list")
+
+subprocess.run(["sudo", "apt-get", "update"])
+
+subprocess.run(["sudo", "apt-get", "-y", "install", "brave-browser"])
