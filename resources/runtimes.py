54af131ba84759e3b434a5442e158204171d496a
thefirethirteen <florea13andrew@gmail.com>
2021-03-04 21:20:56 +0200

    refactor file

--- resources/runtimes.py
+++ resources/runtimes.py
@@ -1,18 +1,18 @@
-#quick-start - a collection of scripts for a quick start
-#Copyright (C) 2021 Andrew F
+# quick-start - a collection of scripts for a quick start
+# Copyright (C) 2021 Andrew F
 
-#This program is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
-#You should have received a copy of the GNU General Public License
-#along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 # runtimes.py
 # version 2.2.0
@@ -22,7 +22,7 @@ import os
 
 os.chdir("runtimes")
 
-#java.sh
+# java.sh
 print("Do you want to install java? [Y/n]")
 USER_INPUT = input()
 if USER_INPUT == "y":
