8aa967a9a612eb8af350d7e1eae64bae7afa9542
thefirethirteen <florea13andrew@gmail.com>
2021-03-07 23:06:27 +0200

    update apps.py

--- resources/apps.py
+++ resources/apps.py
@@ -24,41 +24,48 @@ os.chdir("apps")
 # browser.py
 print("Do you want to install any browsers? [Y/n]")
 USER_INPUT = input()
+
 if USER_INPUT == "y":
     subprocess.run(["python3", "browser.py"])
 
 # office.py
 print("Do you want to install any office suites? [Y/n]")
 USER_INPUT = input()
+
 if USER_INPUT == "y":
     subprocess.run(["python3", "office.py"])
 
 # social.py
 print("Do you want to install any social apps? [Y/n]")
 USER_INPUT = input()
+
 if USER_INPUT == "y":
     subprocess.run(["python3", "social.py"])
 
 # streaming.py
 print("Do you want to install any streaming apps? [Y/n]")
 USER_INPUT = input()
+
 if USER_INPUT == "y":
     subprocess.run(["python3", "streaming.py"])
 
 # games.py
 print("Do you want to install any games? [Y/n]")
 USER_INPUT = input()
+
 if USER_INPUT == "y":
     subprocess.run(["python3", "games.py"])
 
 # development.py
 print("Do you want to install any development apps? [Y/n]")
 USER_INPUT = input()
+
 if USER_INPUT == "y":
     subprocess.run(["python3", "development.py"])
 
 # miscellaneous.py
 print("Do you want to install any miscellaneous apps? [Y/n]")
 USER_INPUT = input()
+
 if USER_INPUT == "y":
     subprocess.run(["python3", "miscellaneous.py"])
