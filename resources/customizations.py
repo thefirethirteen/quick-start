30b79599fc51149a750fe492ec88e6f48dbf2fa7
thefirethirteen <florea13andrew@gmail.com>
2021-03-07 23:09:21 +0200

    update customizations.py

--- resources/customizations.py
+++ resources/customizations.py
@@ -24,17 +24,20 @@ os.chdir("customizations")
 # wallpapers.py
 print("Do you want to add any wallpapers? [y/n]")
 USER_INPUT = input()
+
 if USER_INPUT == "y":
     subprocess.run(["python3", "wallpapers.py"])
 
 # icons.py
 print("Do you want to add any icons? [y/n]")
 USER_INPUT = input()
+
 if USER_INPUT == "y":
     subprocess.run(["python3", "icons.py"])
 
 # fonts.py
 print("Do you want to add any fonts? [y/n]")
 USER_INPUT = input()
+
 if USER_INPUT == "y":
     subprocess.run(["python3", "fonts.py"])
